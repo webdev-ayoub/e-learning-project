81a61aeb8b452d24e68389d7cea3d910e76714de
Ayoub Aberbach <ayoub_aberbach@yahoo.com>
2023-03-27 14:01:11 +0100

    modified:   src/components/Login.jsx 	modified:   src/components/Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,27 +5,30 @@ import NavBar from './NavBar';
 function Register() {
    return (
       <>
-         <NavBar />
-         <div className='container mt-5 pt-5 w-auto mx-auto'>
-            <div className='container w-75 mx-auto bg-info p-3 rounded'>
-               <h1 className='text-center bg-white rounded p-2 w-auto'>Sign Up</h1>
-               <form>
-                  <div className="form-floating mb-3">
-                     <input type="text" className="form-control" id="username" placeholder="John Doe" />
-                     <label htmlFor="username">Username</label>
+         <div className='container'>
+            <div className='min-vh-100 d-flex flex-column'>
+               <div className="card m-auto">
+                  <h1 className='card-header text-center fw-bold text-center rounded p-2 w-auto'>Sign Up</h1>
+                  <div class="card-body">
+                     <form className='w-auto'>
+                        <div className="form-floating mb-3">
+                           <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                           <label htmlFor="floatingInput">Username</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                           <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                           <label htmlFor="floatingInput">Email</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                           <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                           <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <div className='d-flex gap-2'>
+                           <button type='submit' className='btn btn-dark w-100'>Register</button>
+                        </div>
+                     </form>
                   </div>
-                  <div className="form-floating mb-3">
-                     <input type="email" className="form-control" id="userEmail" placeholder="name@example.com" />
-                     <label htmlFor="userEmail">Email</label>
-                  </div>
-                  <div className="form-floating mb-3">
-                     <input type="password" className="form-control" id="userPassword" placeholder="Password" />
-                     <label htmlFor="userPassword">Password</label>
-                  </div>
-                  <div className='d-flex gap-2'>
-                     <button type='submit' className='btn btn-dark w-100'>Register</button>
-                  </div>
-               </form>
+               </div>
             </div>
          </div>
       </>
